9cbeb35e0d4fd00704f1e02f0dc63b2cb31a28d9
Zaib321 <161988392+Zaib321@users.noreply.github.com>
2024-03-08 05:25:37 +0500

    NodeJS_Q.9

---
+++ Q.9.js
@@ -0,0 +1,5 @@
+"use strict";
+// Q.9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let favoritenumber = 4;
+console.log(`"My favorite num is ${favoritenumber}."`);
---
+++ Q.9.ts
@@ -0,0 +1,4 @@
+// Q.9 Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoritenumber: number= 4;
+console.log(`"My favorite num is ${favoritenumber}."`);
